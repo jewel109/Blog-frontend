d2f7d93635dc02670553911f9c992d09f8ab8f78
jewel109 <jewelrana176517@gmail.com>
2022-08-28 23:12:05 +0600

    some file added

--- server/controllers/story/story.js
+++ server/controllers/story/story.js
@@ -13,14 +13,37 @@ const addStory = ErrorWrapper( async (req, res, next) => {
 
   let wordCount = content.trim().split(/\s+/).length
 
+  // The trim() method removes whitespace from both sides of a string.
+  // The split() method splits a string into an array of substrings.
+  //
+  // The split() method returns the new array.
+  //
+  // The split() method does not change the original string.
+
+  let readTime = Math.floor(wordCount/200)
+  try{
+    const newStory = await Story.create({
+      title,
+      content,
+      author:req.user._id,
+      readTime
+    })
+    return res.status(200).json({
+      success:true,
+      message:"Story added successfully",
+      data:newStory
+    })
+  }catch(err){
+    return next(err)
+  }
+  
 
 })
 
-// The trim() method removes whitespace from both sides of a string.
-// The split() method splits a string into an array of substrings.
-//
-// The split() method returns the new array.
-//
-// The split() method does not change the original string.
+const getAllStories = ErrorWrapper( async (req, res, next) => {
+  
+})
+
+
 
 module.exports = router
