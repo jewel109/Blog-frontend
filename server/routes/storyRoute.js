495939c19a990a5e036c4259b1547b56c239219b
jewel109 <jewelrana176517@gmail.com>
2022-09-09 12:09:43 +0600

    story route functionality added

--- server/routes/storyRoute.js
+++ server/routes/storyRoute.js
@@ -2,6 +2,20 @@ const express = require("express")
 
 const router = express.Router()
 
+const {getAccessToRoute} = require("../middlewares/auth/accessRoute")
+
+
+const {addStory,editStory,deleteStory,editStoryPage,likeStory,detailStory,getAllStories} = require("../controllers/story/story")
+
+const {checkStoryExist, checkUserAndStoryExist} = require("../middlewares/database/databaseErrorHandler")
+
+
+router.post("/addstory",[getAccessToRoute,addStory] )
+
+router.post("/:slug", checkStoryExist,detailStory)
+
+router.post("/:slug/like",[getAccessToRoute,checkStoryExist],likeStory)
+
 
 
 module.exports = router
