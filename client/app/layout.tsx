8657f665ec366aec836aba0978d1163aff0622f9
jewel109 <raihanurrahmanjewel@gmail.com>
2024-02-15 15:41:43 +0600

    home page refactored

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container mx-auto p-4  rounded-lg shadow-md w-6/12">
+        <div className="container mx-auto p-4  rounded-lg shadow-md w-11/12">
           {/* Your content here */}
           {children}
 
