7eafa70bf90725bb00da8581402a276927266499
jewel109 <raihanurrahman@gmail.com>
2024-02-15 14:07:15 +0600

    authtoken refactored

--- server/routes/storyRoute.js
+++ server/routes/storyRoute.js
@@ -2,26 +2,26 @@ const express = require("express")
 
 const router = express.Router()
 
-const {getAccessToRoute} = require("../middlewares/auth/accessRoute")
+const { getAccessToRoute } = require("../middlewares/auth/accessRoute")
 
 
-const {addStory,editStory,deleteStory,editStoryPage,likeStory,detailStory,getAllStories} = require("../controllers/story/story")
+const { addStory, editStory, deleteStory, editStoryPage, likeStory, detailStory, getAllStories } = require("../controllers/story/story")
 
-const {checkStoryExist, checkUserAndStoryExist} = require("../middlewares/database/databaseErrorHandler")
+const { checkStoryExist, checkUserAndStoryExist } = require("../middlewares/database/databaseErrorHandler")
 
 
-router.post("/addstory",[getAccessToRoute,addStory] )
+router.post("/addstory", [getAccessToRoute, addStory])
 
-router.post("/:slug", checkStoryExist,detailStory)
+router.post("/:slug", checkStoryExist, detailStory)
 
-router.post("/:slug/like",[getAccessToRoute,checkStoryExist],likeStory)
+router.post("/:slug/like", [getAccessToRoute, checkStoryExist], likeStory)
 
-router.get("/editstory/:slug",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist],editStoryPage)
+router.get("/editstory/:slug", [getAccessToRoute, checkStoryExist, checkUserAndStoryExist], editStoryPage)
 
-router.put("/:slug/edit",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist],editStory)
+router.put("/:slug/edit", [getAccessToRoute, checkStoryExist, checkUserAndStoryExist], editStory)
 
-router.delete("/:slug/delete",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist],deleteStory)
+router.delete("/:slug/delete", [getAccessToRoute, checkStoryExist, checkUserAndStoryExist], deleteStory)
 
-router.get("/getallstories",getAllStories)
+router.get("/getallstories", getAllStories)
 
 module.exports = router
