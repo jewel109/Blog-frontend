28443dac751751c1e18c87a55c22ebfbf7025a97
jewel109 <raihanurrahmanjewel@gmail.com>
2024-02-19 21:26:46 +0600

    commnetRoute added

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,9 +7,11 @@ const router = express.Router()
 const authRoute = require('./auth')
 const userRoute = require('./userRoute')
 const storyRoute = require('./storyRoute')
+const commentRoute = require('./commentRoute')
 
-router.use("/auth",authRoute)
-router.use('/user',userRoute)
+router.use("/auth", authRoute)
+router.use('/user', userRoute)
 router.use('/story', storyRoute)
+router.use('/comment', commentRoute)
 
 module.exports = router
