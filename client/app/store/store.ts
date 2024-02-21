c636d802e3f8400124520b6907dbab8d68768e46
jewel109 <raihanurrahmanjewel@gmail.com>
2024-02-21 22:15:52 +0600

    userReducer is configured in store

--- client/app/store/store.ts
+++ client/app/store/store.ts
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import userReducer from "../features/userSlice"
+import { useDispatch } from 'react-redux'
 export const store = configureStore({
   reducer: {
+    userReducer: userReducer
   }
 })
+export const useAppDispatch = () => useDispatch<typeof store.dispatch>()
+export type RootState = ReturnType<typeof store.getState>
