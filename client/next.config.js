251f621df73fc353da5b16ec47d725c0dbca4cc0
jewel109 <raihanurrahmanjewel@gmail.com>
2024-03-12 12:42:28 +0600

    reactStrictMode in next config is disabled

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+
 
 module.exports = {
-  plugins:
-    [new MiniCssExtractPlugin()]
-
-  ,
   reactStrictMode: false,
-
 }
+
