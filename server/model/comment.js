68e04e713bfb1f01952abe733024070309ee5e80
jewel109 <raihanurrahmanjewel@gmail.com>
2024-04-01 16:06:44 +0600

    refModel enum and replies field is added in comment schema

--- server/model/comment.js
+++ server/model/comment.js
@@ -5,7 +5,12 @@ const CommentSchema = new mongoose.Schema({
   story: {
     type: mongoose.Schema.ObjectId,
     required: true,
-    ref: "Story"
+    ref: 'refModel'
+  },
+  refModel: {
+    type: String,
+    required: true,
+    enum: ['Story', 'Comment']
   },
 
   content: {
@@ -20,6 +25,10 @@ const CommentSchema = new mongoose.Schema({
   likes: [{
     type: mongoose.Schema.ObjectId,
     ref: "User"
+  }],
+  replies: [{
+    type: mongoose.Types.ObjectId,
+    ref: "Comment"
   }],
   likeCount: {
     type: Number,
