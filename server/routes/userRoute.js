4bc727b9d64e9265f4da0ebe229d55000299cdd1
jewel109 <raihanurrahmanjewel@gmail.com>
2024-04-24 09:44:40 +0600

    route for totalLikedStory

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,15 +1,16 @@
 const express = require('express')
-const { profile, editProfile, changePassword, addStoryToReadList, readListPage } = require('../controllers/user/user')
+const { profile, editProfile, changePassword, addStoryToReadList, readListPage, totalLikedStory } = require('../controllers/user/user')
 const { getAccessToRoute } = require('../middlewares/auth/accessRoute')
 
 const router = express.Router()
 
 //client will request by typing /user/profile or /user/editProfile
-router.get('/profile',getAccessToRoute,profile)
-router.post('/editProfile',getAccessToRoute,editProfile)
-router.put('changePassword',getAccessToRoute,changePassword)
-router.post(':slug/addStoryToReadList',getAccessToRoute,addStoryToReadList)
-router.get('/readList',getAccessToRoute,readListPage)
+router.get('/profile', getAccessToRoute, profile)
+router.post('/editProfile', getAccessToRoute, editProfile)
+router.put('changePassword', getAccessToRoute, changePassword)
+router.post(':slug/addStoryToReadList', getAccessToRoute, addStoryToReadList)
+router.get('/readList', getAccessToRoute, readListPage)
+router.get("/totalLikedStory", totalLikedStory)
 
 
 module.exports = router;
