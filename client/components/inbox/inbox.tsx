a0ada7077ce29e68300e8625637996ce6bebd3c7
jewel109 <raihanurrahmanjewel@gmail.com>
2024-05-21 17:26:40 +0600

    inbox

---
+++ client/components/inbox/inbox.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { ChatBox } from "./chatbox"
+import { ContactList } from "./contactList"
+
+export const InboxPage: React.FC = () => {
+
+  return (
+    <div className="md:p-8 md:pl-12 overflow-hidden">
+      <h2 className="text-3xl font-extrabold text-gray-900 dark:text-gray-300 pb-2 md:pb-12">Messages</h2>
+      <div className="flex flex-row">
+        <div>
+
+          <ContactList />
+        </div>
+        <div className="flex-1">
+          <ChatBox />
+        </div>
+      </div>
+    </div>
+  )
+}
