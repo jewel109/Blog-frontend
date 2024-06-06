a58e41e2fc1a87a2b00114a026748e3ef9a1ded0
jewel109 <raihanurrahmanjewel@gmail.com>
2024-06-06 09:35:13 +0600

    eachUserData

---
+++ client/components/users/eachUserData.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+
+export const EachUserData: React.FC = () => {
+
+  return (
+
+    <li className="py-3 sm:py-4">
+      <div className="flex items-center">
+        <div className="flex-shrink-0">
+          <img className="w-8 h-8 rounded-full" src="" alt="Neil image" />
+        </div>
+        <div className="flex-1 min-w-0 ms-4">
+          <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+            Neil Sims
+          </p>
+          <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+            email@windster.com
+          </p>
+        </div>
+        <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+          $320
+        </div>
+      </div>
+    </li>
+
+  )
+}
