892ee42d9c3ef9095ccae60b400f44e04648a8ba
jewel109 <raihanurrahmanjewel@gmail.com>
2024-06-08 12:11:32 +0600

    inbox

--- client/components/inbox/inbox.tsx
+++ client/components/inbox/inbox.tsx
@@ -9,11 +9,10 @@ export const InboxPage: React.FC = () => {
     <div className="md:p-8 md:pl-12 overflow-hidden">
       <h2 className="text-3xl font-extrabold text-gray-900 dark:text-gray-300 pb-2 md:pb-12">Messages</h2>
       <div className="flex flex-row">
-        <div>
-
+        <div className="md:min-w-96">
           <ContactList />
         </div>
-        <div className="flex-1">
+        <div className="md:flex-1">
           <ChatBox />
         </div>
       </div>
