0b1f907fa3f4845c052073950db9fcbf6dd6310b
jewel109 <raihanurrahmanjewel@gmail.com>
2024-06-08 12:11:43 +0600

    mobile contactlist

---
+++ client/components/inbox/mobileContactList.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { ContactList } from "./contactList"
+
+export const MobileContactList: React.FC = () => {
+
+
+  return (
+    <div>
+      <div>
+        <ContactList />
+      </div>
+    </div>
+  )
+}
