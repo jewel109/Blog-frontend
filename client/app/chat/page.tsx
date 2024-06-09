861c98e347e00137fff99ffc49d6dec84550c055
jewel109 <raihanurrahmanjewel@gmail.com>
2024-06-09 10:37:37 +0600

    tiny change

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -2,7 +2,7 @@ import { InboxPage } from "@/components/inbox/inbox";
 
 export default function Page() {
   return (
-    <div className="md:ml-[210px] md:mt-[84px]">
+    <div className="">
       <InboxPage />
     </div>
   )
