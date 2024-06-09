3de31268b0e4320598db7c4eb1e77cf96659b0a8
jewel109 <raihanurrahmanjewel@gmail.com>
2024-06-09 10:22:48 +0600

    each user

--- client/components/users/eachUserData.tsx
+++ client/components/users/eachUserData.tsx
@@ -1,25 +1,34 @@
 "use client"
 
+import Link from "next/link"
 
-export const EachUserData: React.FC = () => {
+type T = {
+  username: string,
+  email: string
+}
+export const EachUserData: React.FC<T> = ({ username, email }) => {
 
   return (
 
     <li className="py-3 sm:py-4">
       <div className="flex items-center">
         <div className="flex-shrink-0">
-          <img className="w-8 h-8 rounded-full" src="" alt="Neil image" />
+          <img className="w-8 h-8 rounded-full" src="" alt="" />
         </div>
         <div className="flex-1 min-w-0 ms-4">
           <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-            Neil Sims
+            {username}
           </p>
           <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-            email@windster.com
+            {email}
           </p>
         </div>
         <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-          $320
+
+          <Link href="#" className="inline-flex items-center px-2 py-1 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-blue-800">Follow</Link>
+          <Link href="#" className="py-1 px-2 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Message</Link>
+
+
         </div>
       </div>
     </li>
