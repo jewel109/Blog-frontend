3d6d5215e455c8cf7fe1a7bbbcfb5f6704d66667
jewel109 <raihanurrahmanjewel@gmail.com>
2024-07-07 16:42:24 +0600

    testing

--- server/__tests__/app.js
+++ server/__tests__/app.js
@@ -1,18 +1,45 @@
 const request = require("supertest")
-const baseUrl = "http://localhost:5000"
-const db = require('../helpers/mongo-memory-db')
-const app = require('../index')
+const baseUrl = "http://localhost:9000/"
 
-const agent = request.agent(app)
+const mongoose = require("mongoose")
+const express = require("express")
+// beforeAll(async () => {
+//   async function connectingDB() {
+//     try {
+//       mongoose.connect("mongodb://localhost:27017/blog-app-testing");
+//       console.log("db running")
+//     } catch (err) {
+//       console.error(err)
+//     }
+//
+//   }
+//   await connectingDB()
+// })
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  // for (const key in collections) {
+  //   const collection = collections[key];
+  //   await collection.deleteMany({});
+  // }
+  console.log("db cleaned up")
+});
 
-beforeAll(async () => await db.connect())
-beforeEach(async () => await db.clear())
-afterAll(async () => await db.close())
+afterAll(async () => {
+  await mongoose.connection.close();
+  console.log("db closed")
+});
+
+
+
 
 describe("getting authentication route", () => {
-	it("should return succes", async () => {
-		await request(baseUrl)
-		.post("/register")
-		.expect(201)
-	})
+
+
+
+  it("should return succes", async () => {
+    return request(baseUrl)
+      .get("story/getallstories")
+      .expect(200)
+
+  })
 })
